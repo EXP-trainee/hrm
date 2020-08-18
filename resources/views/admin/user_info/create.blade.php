c4d98676b55b07c0635484d9a5dd4f232d019a88
vietoba <buixuanviet.dev@gmail.com>
2020-08-18 17:18:55 +0700

    "Viet%20Push"

--- resources/views/admin/user_info/create.blade.php
+++ resources/views/admin/user_info/create.blade.php
@@ -5,6 +5,169 @@
 @stop
 
 @section('content')
+<div class="content-wrapper">
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <div class="container-fluid">
+		<div class="row">
+		  <div class="col-sm-10">
+			<h1> Create Infomation User Form</h1>
+		  </div>
+		</div>
+	  </div><!-- /.container-fluid -->
+	</section>
+  
+	<!-- Main content -->
+	<section class="content">
+	  <div class="container-fluid">
+		<div class="row">
+		  <!-- left column -->
+		  <div class="col-md-6">
+			<!-- general form elements -->
+			<div class="card card-primary">
+			  <div class="card-header">
+				<h3 class="card-title">Infomation User</h3>
+			  </div>
+			  <!-- /.card-header -->
+			  <!-- form start -->
+			  <form role="form" action="{{route('admin.user-infos.store')}}" method="POST">
+				<div class="card-body">
+				  @csrf
+				  {{-- @method('POST') --}}
+					<div class="form-group">
+						<label for="username">User name</label>
+						<input type="text" name="username" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="avatar">avatar</label>
+						<input type="text" name="avatar" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="phone"> Phone</label>
+						<input type="text" name="phone" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">10 chữ số</small>
+					</div>
+  
+					<div class="form-group">
+					  <label for="phone_2"> phone 2 </label>
+					  <input type="text" name="phone_2" id="" class="form-control" placeholder="" aria-describedby="helpId">
+					  <small id="helpId" class="text-muted">10 chữ số</small>
+				  </div>
+			
+					<div class="form-group">
+						<label for="phone_emergency">phone emergency </label>
+						<input type="text" name="phone_emergency" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="dob">dob</label>
+						<input type="text" name="dob" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="national_id">national id</label>
+						<input type="text" name="national_id" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+  
+					<div class="form-group">
+					  <label for="national_id_image">national id image </label>
+					  <input type="text" name="national_id_image" id="" class="form-control" placeholder="" aria-describedby="helpId">
+					  <small id="helpId" class="text-muted"></small>
+					</div>
+					<div class="form-group">
+						<label for="driving_license">driving license</label>
+						<input type="text" name="driving_license" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="driving_license_image">driving license image</label>
+						<input type="text" name="driving_license_image" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+  
+					<div class="form-group">
+					  <label for="address"> address </label>
+					  <input type="text" name="address" id="" class="form-control" placeholder="" aria-describedby="helpId">
+					  <small id="helpId" class="text-muted"></small>
+				  </div>
+			
+					<div class="form-group">
+						<label for="relationship">Relationship </label>
+						<input type="text" name="relationship" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="banks">bank</label>
+						<input type="text" name="banks" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+			
+					<div class="form-group">
+						<label for="role">role</label>
+						<input type="text" name="role" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
 
+					<div class="form-group">
+						<label for="bio">bio</label>
+						<input type="text" name="bio" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+
+					<div class="form-group">
+						<label for="status">status</label>
+						<input type="text" name="status" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+
+					<div class="form-group">
+						<label for="team_id">team id</label>
+						<input type="text" name="team_id" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted"></small>
+					</div>
+
+					<div class="form-group">
+					  <label for="user_id">User ID</label>
+					  <input type="text" name="user_id" id="" class="form-control" placeholder="" aria-describedby="helpId">
+					  <small id="helpId" class="text-muted">id user dang nhap</small>
+					</div>
+				  {{-- <div class="form-group">
+					<label for="exampleInputFile">File input</label>
+					<div class="input-group">
+					  <div class="custom-file">
+						<input type="file" class="custom-file-input" id="exampleInputFile">
+						<label class="custom-file-label" for="exampleInputFile">Choose file</label>
+					  </div>
+					  <div class="input-group-append">
+						<span class="input-group-text" id="">Upload</span>
+					  </div>
+					</div>
+				  </div>  --}}
+				  
+				</div>
+				<!-- /.card-body -->
+  
+				<div class="card-footer">
+				  <button type="submit" class="btn btn-primary">Create</button>
+				</div>
+			  </form>
+			</div>
+			<!-- /.card -->           
+		</div>
+		<!-- /.row -->
+	  </div><!-- /.container-fluid -->
+	</section>
+	<!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
 	
 @stop
