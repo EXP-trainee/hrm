aaaed4eaae5c143a4f51b55b96215a7c87300125
Nic <tminh94@gmail.com>
2020-08-18 10:54:58 +0700

    "Nic"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,9 +20,21 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('phone_2')->nullable();
+            $table->string('phone_emergency')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('national_id')->nullable();
+            $table->string('national_id_image')->nullable();
+            $table->string('driving_license')->nullable();
+            $table->string('driving_license_image')->nullable();
+            $table->text('address')->nullable();
+            $table->json('banks')->nullable();
             $table->integer('role')->default(0);
             $table->text('bio')->nullable();
+            $table->tinyInteger('status');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
