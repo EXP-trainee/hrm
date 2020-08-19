46f0a520ecbd896b35f8e599df5ae9e003854896
vietoba <buixuanviet.dev@gmail.com>
2020-08-19 15:35:50 +0700

    "Viet%20Push"

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Auth::routes();
+
+/*
+|------------------------------------------------------------------------------------
+| Admin
+|------------------------------------------------------------------------------------
+*/
+Route::group(['prefix' => ADMIN, 'as' => ADMIN . '.'], function () {
+    Route::get('/', 'DashboardController@index')->name('dash');
+    Route::resource('users', 'UserController');
+    Route::resource('user-infos', 'UserInfoController');
+    Route::resource('teams', 'TeamController');
+
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/showuserinfo', 'UserController@showuserinfo')->name('showuserinfo');
+
+Route::post('/file','UserController@doUpload')-> name('file');
