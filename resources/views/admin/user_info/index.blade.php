e7eafb92f8e6938dc756b059f86a20287d2bb5b0
vietoba <buixuanviet.dev@gmail.com>
2020-08-27 10:05:06 +0700

    "Viet%20Push"

--- resources/views/admin/user_info/index.blade.php
+++ resources/views/admin/user_info/index.blade.php
@@ -6,114 +6,102 @@
 
 @section('content')
 
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-12">
+          <div class="col-sm-3"></div>
+          <div class="col-sm-6">
             <h1>List Infomation User</h1>
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">List Infomation User</li>
-            </ol>
-          </div>
+          <div class="col-sm-3"></div>
         </div>
       </div><!-- /.container-fluid -->
+      <br>
     </section>
 
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <!-- /.row -->
-        <div class="row">
-          <div class="col-md-sm-7">
-            <div class="card">
-              <div class="card-header">
-                <div class="col-md-sm-3">
-                  <div class="card"> 
-                      <h2 style="text-align: center">{{$userinfo->name}}</h2>
-                      <div class="card" style="width:400px">
-                        <img class="card-img-top" src="{{$userinfo->avatar}}" alt="Card image" style="width:100%">
-                      </div>
-                  </div>
-                </div>
-                <!-- /.card -->    
-               
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <div class="card" style="width:400px;"> 
+              <h2 style="text-align: center"> {{$userinfo->name}} </h2>
+              <div class="card" style="width:400px">
+                <img class="card-img-top" src="{{$userinfo->avatar}}" alt="Card image" style="width:100%">
               </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table class="table table-bordered table-responsive" >
-                    <tr>
-                      <th>STT</th>
-                      <td>{{$userinfo->id}}</td>
-                    </tr>
-                    <tr>
-                      <th>Email</th>
-                      <td>{{$userinfo->email}}</td>
-                    </tr>
-                    <tr>
-                      <th>Avatar</th>
-                      <td>{{$userinfo->avatar}}</td>
-                    </tr>
-                    <tr>
-                      <th>Phone</th>
-                      <td><div>{{$userinfo->phone}}</div>
-                          <div>{{$userinfo->phone_2}}</div>
-                          <div>{{$userinfo->phone_emergency}}</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>Dob</th>
-                      <td>{{$userinfo->dob}}</td>
-                    </tr>
-                    <tr>
-                      <th>National ID</th>
-                      <td>{{$userinfo->national_id}}</td>
-                    </tr>
-                    <tr>
-                      <th>Driving license image</th>
-                      <td>{{$userinfo->driving_license_image}}</td>
-                    </tr>
-                    <tr>
-                      <th>Address</th>
-                      <td>{{$userinfo->address}}</td>
-                    </tr>
-                    <tr>
-                      <th>Relationship</th>
-                      <td>{{$userinfo->relationship}}</td>
-                    </tr>
-                    <tr>
-                      <th>Banks</th>
-                      <td>{{$userinfo->banks}}</td>
-                    </tr>
-                    <tr>
-                      <th>Bio</th>
-                      <td>{{$userinfo->bio}}</td>
-                    </tr>
-                    <tr>
-                      <th>Status</th>
-                      <td>{{$userinfo->status}}</td>
-                    </tr>
-                    <tr>
-                      <th>Team</th>
-                      <td>{{$userinfo->team?$userinfo->team->name:""}}</td>
-                    </tr>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <table class="table table-bordered table-responsive" >
+                  <tr>
+                    <th>STT</th>
+                    <td>{{$userinfo->id}}</td>
+                  </tr>
+                  <tr>
+                    <th>Email</th>
+                    <td>{{$userinfo->email}}</td>
+                  </tr>
+                  <tr>
+                    <th>Avatar</th>
+                    <td>{{$userinfo->avatar}}</td>
+                  </tr>
+                  <tr>
+                    <th>Phone</th>
+                    <td><div>{{$userinfo->phone}}</div>
+                        <div>{{$userinfo->phone_2}}</div>
+                        <div>{{$userinfo->phone_emergency}}</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Dob</th>
+                    <td>{{$userinfo->dob}}</td>
+                  </tr>
+                  <tr>
+                    <th>National ID</th>
+                    <td>{{$userinfo->national_id}}</td>
+                  </tr>
+                  <tr>
+                    <th>Driving license image</th>
+                    <td>{{$userinfo->driving_license_image}}</td>
+                  </tr>
+                  <tr>
+                    <th>Address</th>
+                    <td>{{$userinfo->address}}</td>
+                  </tr>
+                  <tr>
+                    <th>Relationship</th>
+                    <td>{{$userinfo->relationship}}</td>
+                  </tr>
+                  <tr>
+                    <th>Banks</th>
+                    <td>{{$userinfo->banks}}</td>
+                  </tr>
+                  <tr>
+                    <th>Bio</th>
+                    <td>{{$userinfo->bio}}</td>
+                  </tr>
+                  <tr>
+                    <th>Status</th>
+                    <td>{{$userinfo->status}}</td>
+                  </tr>
+                  <tr>
+                    <th>Team</th>
+                    <td>{{$userinfo->team?$userinfo->team->name:""}}</td>
+                  </tr>
                 </table>
 
                 <a href="{{route('admin.users.edit',['user' => $userinfo->id])}}" class="btn btn-primary"><i class="fa fa-edit " aria-hidden="true"> Update infomation</i></a>
               </div> 
-              <!-- /.card-body -->
+             
             </div>
-          </div>
-        
-          <!-- /.card -->
 
           </div>
+
         </div>
         <!-- /.row -->
         
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,14 @@
-@extends('layouts.app')
+@extends('admin.default')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+            <a href="{{ route('getuser') }}" class="btn btn-primary">Information</a>
+            <a href="#" class="btn btn-success">Team</a>
+            <a href="#" class="btn btn-danger">Salary</a>
+            <a href="#" class="btn btn-warning">Workdays</a>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
