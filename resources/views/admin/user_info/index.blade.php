f1f5a8ae8726297007234f436db504ee963d4f23
vietoba <buixuanviet.dev@gmail.com>
2020-09-03 14:33:57 +0700

    "Viet%20Push"

--- resources/views/admin/user_info/index.blade.php
+++ resources/views/admin/user_info/index.blade.php
@@ -88,7 +88,8 @@
                   </tr>
                 </table>
 
-                <a href="{{route('admin.users.edit',['user' => $userinfo->id])}}" class="btn btn-primary"><i class="fa fa-edit " aria-hidden="true"> Update infomation</i></a>
+                {{-- <a href="{{route('admin.users.edit',['user' => $userinfo->id])}}" class="btn btn-primary"><i class="fa fa-edit " aria-hidden="true"> Update infomation</i></a> --}}
+                <button type="button" class="btn btn-info editbtn" id="myBtn" > UPDATE INFOMATION</button>
               </div> 
              
             </div>
@@ -99,8 +100,204 @@
         <!-- /.row -->
         
       </div><!-- /.container-fluid -->
+
+      
+      <!-- Modal EDIT-->
+      <div class="modal fade" id="editModal" role="dialog">
+        <div class="modal-dialog modal-lg">
+        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title" class="float-left">Update InfoUser</h4>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            
+            </div>
+            <div class="modal-body">
+              <form  role="form" action="{{route('admin.users.update',['user'=>$userinfo->id])}}" method="POST" enctype="multipart/form-data" >
+                @method('PUT')
+                @csrf
+                <div class="card-body">
+                  @csrf
+                  <div class="form-row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="name">Name</label>
+                        <input value="{{$userinfo->name}}" type="text" name="name" id="name" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted">full name</small>
+                      </div>
+
+                      <div class="form-group">
+                        <label for="email">email</label>
+                        <input value="{{$userinfo->email}}" type="text" name="email" id="email" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+                      
+                      
+                      <div class="form-group">
+                        <label for="avatar">avatar</label>
+                        <input type="file" name="avatar" id="avatar" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+              
+                      <div class="form-group">
+                          <label for="phone"> phone</label>
+                          <input value="{{$userinfo->phone}}" type="text" name="phone" id="phone" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted">10 số</small>
+                      </div>
+
+                      <div class="form-group">
+                        <label for="phone_2"> phone_2 </label>
+                        <input value="{{$userinfo->phone_2}}" type="text" name="phone_2" id="phone_2" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                    </div>
+              
+                      <div class="form-group">
+                          <label for="phone_emergency">phone emergency </label>
+                          <input value="{{$userinfo->phone_emergency}}" type="text" name="phone_emergency" id="phone_emergency" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted"></small>
+                      </div>
+              
+                      <div class="form-group">
+                          <label for="dob">dob</label>
+                          <input value="{{$userinfo->dob}}" type="date" name="dob" id="dob" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted">mm/dd/yyyy</small>
+                      </div>
+                      <div class="form-group">
+                        <label for="national_id">national id</label>
+                        <input value="{{$userinfo->national_id}}" type="text" name="national_id" id="national_id" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+
+                    </div>
+                    <div class="col-md-6">
+
+                      <div class="form-group">
+                        <label for="national_id_image">national id image</label>
+                        <input value="{{$userinfo->national_id_image}}" type="file" name="national_id_image" id="national_id_image" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+                      <div class="form-group">
+                        <label for="driving_license">driving license</label>
+                        <input value="{{$userinfo->driving_license}}" type="text" name="driving_license" id="driving_license" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+        
+                      <div class="form-group">
+                          <label for="driving_license_image">driving license image</label>
+                          <input value="{{$userinfo->driving_license_image}}" type="file" name="driving_license_image" id="driving_license_image" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted"></small>
+                      </div>
+              
+                      <div class="form-group">
+                          <label for="address">address</label>
+                          <input value="{{$userinfo->address}}" type="text" name="address" id="address" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted"></small>
+                      </div>
+      
+                      <div class="form-group">
+                        <label for="relationship">relationship</label>
+                        <input value="{{$userinfo->relationship}}" type="text" name="relationship" id="relationship" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                    </div>
+      
+                      <div class="form-group">
+                          <label for="banks">banks </label>
+                          <input value="{{$userinfo->banks}}" type="text" name="banks" id="banks" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted"></small>
+                      </div>
+              
+                      <div class="form-group">
+                          <label for="bio">bio</label>
+                          <input value="{{$userinfo->bio}}" type="text" name="bio" id="bio" class="form-control" placeholder="" aria-describedby="helpId">
+                          <small id="helpId" class="text-muted"></small>
+                      </div>
+      
+                      <div class="form-group">
+                        <label for="status">status</label>
+                        <input value="{{$userinfo->status}}" type="text" name="status" id="status" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+                      <div class="form-group">
+                        <label for="team">Team</label>
+                        <input value="{{$userinfo->team?$userinfo->team->name:""}}" type="text" name="team" id="team" class="form-control" placeholder="" aria-describedby="helpId">
+                        <small id="helpId" class="text-muted"></small>
+                      </div>
+                      {{-- <div class="form-group">
+                        <label for="my-select">Team</label>
+                        <select id="my-select" class="form-control" name="team_id">
+            
+                          @foreach ($userinfo as $userinfo)
+                        <option value="{{$userinfo->id}}"> {{$userinfo->name}} </option>
+                          @endforeach
+                          
+                        </select>
+                      </div> --}}
+                    </div>
+                  </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">UPDATE</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+
+  <script>
+    $(document).ready(function(){
+      $('.editbtn').on('click', function(){
+        $('#editModal').modal('show');
+
+        // $tr = $(this).closest('tr');
+
+        // var data =$tr.children("td").map(function(){
+        //   return $(this).text();
+        // }).get();
+
+        // console.log(data);
+
+        // $('#id').val(data[0]);
+        // $('#avatar').val(data[1]);
+        // $('#name').val(data[2]);
+        // $('#email').val(data[3]);
+        // $('#phone').val(data[4]);
+        // $('#phone_2').val(data[5]);
+        // $('#phone_emergency').val(data[6]);
+        // $('#dob').val(data[7]);
+        // $('#national_id').val(data[8]);
+        // $('#national_id_image').val(data[9]);
+        // $('#driving_license').val(data[10]);
+        // $('#driving_license_image').val(data[11]);
+        // $('#address').val(data[12]);
+        // $('#relationship').val(data[13]);
+        // $('#banks').val(data[14]);
+        // $('#bio').val(data[15]);
+      });
+
+      $('#editForm').on('submit', function(e){
+        e.preventDefault();
+
+        var id = $('#id').val();
+
+        $ajax({
+          type :"PUT",
+          url:"/users"+id,
+          data: $('#editForm').serialize(),
+        });
+      });
+    });
+  </script>
+
+
 @endsection
+
+
+  
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -217,8 +217,8 @@
                           {{-- <th>Password</th> --}}
 
                           <th>Phone</th>
-                          <th>Phone 2</th>
-                          <th>Phone emergency</th>
+                          {{-- <th>Phone 2</th>
+                          <th>Phone emergency</th> --}}
                           <th>Dob</th>
                           {{-- <th>National ID</th> --}}
                           <th>National ID Image</th>
@@ -248,9 +248,11 @@
                             {{$item->phone_2}}
                             {{$item->phone_emergency}}
                           </td> --}}
-                          <td>{{$item->phone}}</td>
-                          <td>{{$item->phone_2}}</td>
-                          <td>{{$item->phone_emergency}}</td>
+                          <td>
+                            {{$item->phone}}
+                            {{$item->phone_2}}
+                            {{$item->phone_emergency}}
+                          </td>
                           <td>{{$item->dob}}</td>
                           {{-- <td>{{$item->national_id}}</td> --}}
                           <td><img src="{{$item->national_id_image}}" alt=""></td>
@@ -266,9 +268,8 @@
                           {{-- ?là viết tắt của if else --}}
                           <td>{{$item->team?$item->team->name:""}}</td> 
                           <td>
-                              {{-- <a href="{{route('admin.users.edit',['user' => $item->id])}}" class="btn btn-primary"><i class="fa fa-edit " aria-hidden="true"></i></a> --}}
-                              {{-- <a href="{{route('admin.users.edit',['user' => $item->id])}}"><button type="button" class="btn btn-info edit-user " id="myBtn" ><i class="fa fa-edit " aria-hidden="true"></i></button></a> --}}
-                              <button type="button" class="btn btn-info editbtn" id="myBtn" ><i class="fa fa-edit " aria-hidden="true"></i></button>
+                              <a href="{{route('admin.users.edit',['user' => $item->id])}}" class="btn btn-primary"><i class="fa fa-edit " aria-hidden="true"></i></a>
+                              
                               <form action="{{route('admin.users.destroy',['user' => $item->id])}}" method="post">
                                 @csrf
                                 <input type="hidden"  name="_method" value="DELETE">
@@ -288,197 +289,8 @@
         <!-- /.row -->
       </div><!-- /.container-fluid -->
 
-
-
-      <!-- Modal -->
-  <div class="modal fade" id="editModal" role="dialog">
-    <div class="modal-dialog modal-lg">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" class="float-left">Update InfoUser</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-         
-        </div>
-        <div class="modal-body">
-          <form id="editForm" role="form" action="{{route('admin.users.update',['user'=>$item->id])}}" method="POST" enctype="multipart/form-data" >
-            @method('PUT')
-            @csrf
-            <div class="card-body">
-              @csrf
-              <div class="form-row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <label for="name">Name</label>
-                    <input value="{{$item->name}}" type="text" name="name" id="name" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted">full name</small>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="email">email</label>
-                    <input value="{{$item->email}}" type="text" name="email" id="email" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-                  
-                  
-                  <div class="form-group">
-                    <label for="avatar">avatar</label>
-                    <input type="file" name="avatar" id="avatar" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-          
-                  <div class="form-group">
-                      <label for="phone"> phone</label>
-                      <input value="{{$item->phone}}" type="text" name="phone" id="phone" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted">10 số</small>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="phone_2"> phone_2 </label>
-                    <input value="{{$item->phone_2}}" type="text" name="phone_2" id="phone_2" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                </div>
-          
-                  <div class="form-group">
-                      <label for="phone_emergency">phone emergency </label>
-                      <input value="{{$item->phone_emergency}}" type="text" name="phone_emergency" id="phone_emergency" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted"></small>
-                  </div>
-          
-                  <div class="form-group">
-                      <label for="dob">dob</label>
-                      <input value="{{$item->dob}}" type="date" name="dob" id="dob" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted">mm/dd/yyyy</small>
-                  </div>
-                  <div class="form-group">
-                    <label for="national_id">national id</label>
-                    <input value="{{$item->national_id}}" type="text" name="national_id" id="national_id" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-
-                </div>
-                <div class="col-md-6">
-
-                  <div class="form-group">
-                    <label for="national_id_image">national id image</label>
-                    <input value="{{$item->national_id_image}}" type="file" name="national_id_image" id="national_id_image" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-                  <div class="form-group">
-                    <label for="driving_license">driving license</label>
-                    <input value="{{$item->driving_license}}" type="text" name="driving_license" id="driving_license" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-    
-                  <div class="form-group">
-                      <label for="driving_license_image">driving license image</label>
-                      <input value="{{$item->driving_license_image}}" type="file" name="driving_license_image" id="driving_license_image" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted"></small>
-                  </div>
-          
-                  <div class="form-group">
-                      <label for="address">address</label>
-                      <input value="{{$item->address}}" type="text" name="address" id="address" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted"></small>
-                  </div>
-  
-                  <div class="form-group">
-                    <label for="relationship">relationship</label>
-                    <input value="{{$item->relationship}}" type="text" name="relationship" id="relationship" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                </div>
-  
-                  <div class="form-group">
-                      <label for="banks">banks </label>
-                      <input value="{{$item->banks}}" type="text" name="banks" id="banks" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted"></small>
-                  </div>
-          
-                  <div class="form-group">
-                      <label for="bio">bio</label>
-                      <input value="{{$item->bio}}" type="text" name="bio" id="bio" class="form-control" placeholder="" aria-describedby="helpId">
-                      <small id="helpId" class="text-muted"></small>
-                  </div>
-  
-                  <div class="form-group">
-                    <label for="status">status</label>
-                    <input value="{{$item->status}}" type="text" name="status" id="status" class="form-control" placeholder="" aria-describedby="helpId">
-                    <small id="helpId" class="text-muted"></small>
-                  </div>
-  
-                  {{-- <div class="form-group">
-                    <label for="my-select">Team</label>
-                    <select id="my-select" class="form-control" name="team_id">
-        
-                      @foreach ($teams as $item)
-                    <option value="{{$item->id}}"> {{$item->name}} </option>
-                      @endforeach
-                      
-                    </select>
-                  </div>
-                </div> --}}
-              </div>
-            <!-- /.card-body -->
-
-            <div class="card-footer">
-              <button type="submit" class="btn btn-primary">UPDATE</button>
-            </div>
-          </form>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-    </section>
+  </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-
-  <script>
-    $(document).ready(function(){
-      $('.editbtn').on('click', function(){
-        $('#editModal').modal('show');
-
-        $tr = $(this).closest('tr');
-
-        var data =$tr.children("td").map(function(){
-          return $(this).text();
-        }).get();
-
-        console.log(data);
-
-        $('#id').val(data[0]);
-        $('#avatar').val(data[1]);
-        $('#name').val(data[2]);
-        $('#email').val(data[3]);
-        $('#phone').val(data[4]);
-        $('#phone_2').val(data[5]);
-        $('#phone_emergency').val(data[6]);
-        $('#dob').val(data[7]);
-        $('#national_id').val(data[8]);
-        $('#national_id_image').val(data[9]);
-        $('#driving_license').val(data[10]);
-        $('#driving_license_image').val(data[11]);
-        $('#address').val(data[12]);
-        $('#relationship').val(data[13]);
-        $('#banks').val(data[14]);
-        $('#bio').val(data[15]);
-      });
-
-      // $('#editForm').on('submit', function(e){
-      //   e.preventDefault();
-
-      //   var id = $('#id').val();
-
-      //   $ajax({
-      //     type :"PUT",
-      //     url:"/users"+id,
-      //     data: $('#editForm').serialize(),
-      //   });
-      // });
-    });
-  </script>
-
 @endsection
