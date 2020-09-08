ea3ac4583ebb8231b1f457b617a0cbde78a79e34
Nic <tminh94@gmail.com>
2020-09-08 16:24:44 +0700

    "Nic"

--- public/git.php
+++ public/git.php
@@ -1,3 +1,3 @@
 <?php
-$out = shell_exec( 'cd ..;git reset --hard origin;git pull origin master 2>&1' );
+$out = shell_exec( 'cd ..;git clean -f -d;git reset --hard origin;git pull origin master 2>&1' );
 echo '<pre>'.$out.'</pre>';
