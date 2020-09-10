e1272c2d80323fe0cf207458d1c31cbb3a49ed81
vietoba <buixuanviet.dev@gmail.com>
2020-09-10 17:07:09 +0700

    reset password

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-
+	public const DASH = '/';
+	public const USER = '/getuser';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
-Auth::routes();
+//Auth::routes();
+Auth::routes([
+	'register' => false,
+	'verify' => true,
+]);
 
 /*
 |------------------------------------------------------------------------------------
