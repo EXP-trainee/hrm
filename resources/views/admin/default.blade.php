d843a4baa21272fd1512b0bfc542b54b9769cc7f
Nic <tminh94@gmail.com>
2020-09-10 09:33:49 +0700

    "Nic"

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -5,49 +5,49 @@
 
 <body class="app">
 
-    @include('admin.partials.spinner')
+@include('admin.partials.spinner')
 
-    <div>
-        <!-- #Left Sidebar ==================== -->
-        @include('admin.partials.sidebar')
+<div>
+	<!-- #Left Sidebar ==================== -->
+@include('admin.partials.sidebar')
 
-        <!-- #Main ============================ -->
-        <div class="page-container">
-            <!-- ### $Topbar ### -->
-            @include('admin.partials.topbar')
+<!-- #Main ============================ -->
+	<div class="page-container">
+		<!-- ### $Topbar ### -->
+	@include('admin.partials.topbar')
 
-            <!-- ### $App Screen Content ### -->
-            <main class='main-content bgc-grey-100'>
-                <div id='mainContent'>
-                    <div class="container-fluid">
+	<!-- ### $App Screen Content ### -->
+		<main class='main-content bgc-grey-100'>
+			<div id='mainContent'>
+				<div class="container-fluid">
 
-                        <h4 class="c-grey-900 mT-10 mB-30">@yield('page-header')</h4>
+					<h4 class="c-grey-900 mT-10 mB-30">@yield('page-header')</h4>
 
-                        @include('admin.partials.messages')
-                        @yield('content')
+					@include('admin.partials.messages')
+					@yield('content')
 
-                    </div>
-                </div>
-             </main>
+				</div>
+			</div>
+		</main>
 
-            <!-- ### $App Screen Footer ### -->
-            <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
+		<!-- ### $App Screen Footer ### -->
+		<footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
                 <span>Copyright © {{ date('Y') }} Designed by
                     <a href="https://colorlib.com" target='_blank' title="Colorlib">Colorlib</a>. All rights reserved.</span>
-            </footer>
-        </div>
-    </div>
+		</footer>
+	</div>
+</div>
 
-    <script src="{{ mix('/js/app.js') }}?v=1"></script>
+<script src="{{ mix('/js/app.js') }}?v=1"></script>
 
 {{--    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>--}}
 {{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--}}
 {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>--}}
-    
 
-    @yield('js')
 
-    @stack('scripts')
+@yield('js')
+
+@stack('scripts')
 </body>
 
 </html>
